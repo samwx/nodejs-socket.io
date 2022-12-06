d5719828fb5e57049a24658daa8047ca27e28048
samwx <samuelmartins.sw@gmail.com>
2022-12-05 21:35:05 -0300

    Update packages

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
-const express = require('express')
-const app = express()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
-const path = require('path')
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-io.on('connection', (socket) => {
-    console.log(`User connected with id ${socket.id}`)
-})
+let users = [];
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
+io.on("connection", (socket) => {
+    console.log(`User connected with id ${socket.id}`);
+    users = users.concat(socket.id);
+
+    console.log(`Connected users: ${users}`);
+
+    socket.on("clientMessage", (msgContent) => {
+        socket.emit("recebeuMsg", "Recebeu msg");
+
+        socket.broadcast.emit("groupMessage", msgContent);
+    });
+});
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
 
 http.listen(3000, () => {
-  console.log('Listening on port 3000')
-})
+    console.log("Listening on port 3000");
+});
